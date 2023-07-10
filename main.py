a52e3d903ea6e4c2375a25060880cb249f9b1023
Legion <estebandsilva@github.com>
2023-07-10 12:50:11 +0200

    Loop done + conteo pwm

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import threading
 from Motor_Class import *
 
 def pwm_task():
-    pass
+    sleep(0)
 
 def print_task():
     print("Pulses=", motor_X.total_pulses, " Position=", motor_X.position)
