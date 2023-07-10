63ff214f673a7b42ad505519b52673006d4e1949
Legion <estebandsilva@github.com>
2023-07-10 12:49:06 +0200

    Loop done + conteo pwm

--- main.py
+++ main.py
@@ -14,11 +14,7 @@ def print_task():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test Begin")
-    pwm_thread = threading.Thread(target=pwm_task)
-    print_thread = threading.Thread(target=print_task)
 
-    pwm_thread.start()
-    print_thread.start()
 
 
     try:
@@ -27,6 +23,12 @@ if __name__ == '__main__':
             #print("Test Begin")
             #motor_X.foward()
             #print("Pulses=", motor_X.total_pulses, " Position=", motor_X.position)
+            pwm_thread = threading.Thread(target=pwm_task)
+            print_thread = threading.Thread(target=print_task)
+
+            pwm_thread.start()
+            print_thread.start()
+
             pwm_thread.join()
             print_thread.join()
 
