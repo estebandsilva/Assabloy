c0d65a22fe4aadbf17b77f812caca64fb9b1ad8c
Legion <estebandsilva@github.com>
2023-07-12 11:41:46 +0200

    cpu low

--- Sequencia_Class.py
+++ Sequencia_Class.py
@@ -1,10 +1,8 @@
 from Motor_Class import *
 
-
-motor_X = Motor(ENA = 14, PUL_out = 18, DIR_out = 15, PUL_in = 12, DIR_in = 7 , SW_ini = 23, SW_fin = 24)
-#motor_Y = Motor(ENA = 14, PUL_out = 18, DIR_out = 15, PUL_in = 12, DIR_in = 7 , SW_ini = 23, SW_fin = 24)
-
 class Sequencia(Motor):
     def __init__(self, ):
         self.motor_X = Motor(ENA = 14, PUL_out = 18, DIR_out = 15, PUL_in = 12, DIR_in = 7 , SW_ini = 23, SW_fin = 24)
         #self.motor_Y = Motor(ENA = 14, PUL_out = 18, DIR_out = 15, PUL_in = 12, DIR_in = 7 , SW_ini = 23, SW_fin = 24)
+
+sequencia = Sequencia()
