c787072d47359c27c9e4655529176de6f38e0d3b
jeremi-sanchez <jeremis46@gmail.com>
2023-07-17 14:49:04 +0200

    Prueba Motor 1

--- Sequencia_Class.py
+++ Sequencia_Class.py
@@ -15,7 +15,7 @@ class Sequencia:
         #sleep(10)
         #self.motor_X.stop()
         self.motor_X.calibration()
-        sleep(10)
+        sleep(0.5)
         self.motor_X.stop()
         sleep(10)
         print("Done")
