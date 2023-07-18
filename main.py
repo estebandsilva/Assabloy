cf6043473c16c09011e64c25ccaffc891936da57
Legion <estebandsilva@github.com>
2023-07-18 12:11:12 +0200

    Prueba Git

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import threading
 import subprocess
 import sys
-#subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 
 from Sequencia_Class import *
