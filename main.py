59d60844b72be6cfaef95a07e9b79dc9b32ac7c7
jere_ideapad <jeremis46@gmail.com>
2023-08-21 12:56:24 +0200

    Git Jere

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
             #print("Test Begin")
 
             #print("Pulses=", motor_X.total_pulses, " Position=", motor_X.position)
-            print("Pulses=", sequencia.motor_X.total_pulses, " Position=", round(sequencia.motor_X.position,2)
+            print("Pulses=", sequencia.motor_X.total_pulses, " Position=", round(sequencia.motor_X.position,2))
             #pwm_thread = threading.Thread(target=pwm_task)
             #print_thread = threading.Thread(target=print_task)
 
