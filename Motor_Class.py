2f12d94e67cac93303da35035b455655bcbc02b8
jere_ideapad <jeremis46@gmail.com>
2023-09-28 13:46:38 +0200

    Git Jere

--- Motor_Class.py
+++ Motor_Class.py
@@ -179,7 +179,7 @@ class Motor:
         self.movement = False
         #GPIO.output(self._ENA, GPIO.LOW)
         #self.pwm.stop()
-        self.pwm.set_pwm(self.PUL_pwm, 0, 0)
+        self.pwm.set_pwm(self.PUL_pwm, 100, 0)
         #sleep(0.001)
 
 
