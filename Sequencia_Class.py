d58a4d5d6cf4f12677278a5c03405ad215e52ea3
jere_ideapad <jeremis46@gmail.com>
2023-09-30 20:45:32 +0200

    Git Jere

--- Sequencia_Class.py
+++ Sequencia_Class.py
@@ -160,15 +160,15 @@ class Sequencia:
     def trajectory_Y(self,Y_bool):
         if Y_bool!=self.motor_Y.direction:
             #print("Entering",Y_bool,self.motor_Y.direction)
-            self.stop()
-            # self.motor_Y.stop()
+            #self.stop()
+            self.motor_Y.stop()
             if self.motor_X.direction:
                 self.motor_X.foward()
             else:
                 self.motor_X.backward()
             sleep(1.5)
-            #self.motor_X.stop()
-            self.stop()
+            self.motor_X.stop()
+            #self.stop()
             if self.motor_Y.direction:
                 self.motor_Y.foward()
             else:
